e71c0154db0b8ad73d9c1928a44a7978af9ca99b
AndrewPolengolas <andrewnd2009@outlook.com>
2025-03-22 17:29:07 -0300

    Primeiro commit

---
+++ src/main/java/com/polengolas/cache/CacheApplication.java
@@ -0,0 +1,30 @@
+package com.polengolas.cache;
+
+import com.polengolas.cache.services.ProductService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableCaching
+public class CacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheApplication.class, args);
+	}
+
+
+	@Bean
+	ApplicationRunner runner(ProductService productService){
+		return args -> {
+			System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n");
+			System.out.println("Id: 1: " + productService.getById(1L));
+			System.out.println("Id: 2: " + productService.getById(2L));
+			System.out.println("Id: 1: " + productService.getById(1L));
+			System.out.println("Id: 1: " + productService.getById(1L));
+			System.out.println("Id: 1: " + productService.getById(1L));
+		};
+	}
+}
---
+++ src/main/java/com/polengolas/cache/model/Product.java
@@ -0,0 +1,49 @@
+package com.polengolas.cache.model;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+    private Long id;
+    private String category;
+    private String name;
+
+    public Product(Long id, String category, String name) {
+        this.id = id;
+        this.category = category;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", category='" + category + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
